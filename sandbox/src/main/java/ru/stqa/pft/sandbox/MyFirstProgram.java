7852bac57b56bfa90ce568ba322fd72dd4ff1d6f
SvitlanaSav <sav7805@gmail.com>
2019-06-25 12:20:03 +0200

    создан пакет для моей первой программы

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram {
+
+  public static void main(String[] args) {
+
+      System.out.println("ru.stqa.pft.sandbox.MyFirstProgram!");
+
+
+      int x1=6;
+      int x2=6;
+      int y1=8;
+      int y2=3;
+      System.out.println("Расстояние между точками на двумерной плоскости"  = "  Math.sqrt(Math.pow(x1-x2)+Math.pow(y1-y2)));
+
+
+  }
+
+
+}
