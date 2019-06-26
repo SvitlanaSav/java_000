1305aec6757aeaf1fd2356a51e0b59eb2146c10c
SvitlanaSav <sav7805@gmail.com>
2019-06-26 15:32:26 +0200

    расстояние между точками шаг1

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,11 +7,19 @@ public class MyFirstProgram {
       System.out.println("ru.stqa.pft.sandbox.MyFirstProgram!");
 
 
-      int x1=6;
+      int x1=10;
       int x2=6;
       int y1=8;
       int y2=3;
-      System.out.println("Расстояние между точками на двумерной плоскости"  = "  Math.sqrt(Math.pow(x1-x2)+Math.pow(y1-y2)));
+      int a=2;
+      int q=x1-x2;
+      int w=y1-y2;
+
+
+
+
+      System.out.println("расстояние между точками p1 и p2 на двумерной плоскости" + "l" + Math.sqrt (Math.pow(a,q) + Math.pow(a,w)));
+
 
 
   }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point1.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.sandbox;
+
+public class Point1 {
+
+    public static double distance ;
+
+
+    public double x1=6;
+    public double y1=8;
+
+
+
+
+
+
+}
