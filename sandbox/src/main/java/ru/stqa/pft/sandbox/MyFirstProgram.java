0ede3bb74d92daa72b56ddf7a19537a99e4eefe3
SvitlanaSav <sav7805@gmail.com>
2019-06-26 22:22:24 +0200

    добавлены новые функции зад2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,21 +4,23 @@ import static java.lang.Math.*;
 
 public class MyFirstProgram {
 
-    public static void main (String[] args) {
+   public static void main (String[] args) {
 
-        double a=2;
-        double x1=6;
-        double y1=8;
-        double x2=6;
-        double y2=3;
-        double q=x1-x2;
-        double w=y1-y2;
+       Point p = new Point();
 
-        System.out.println("расст. между двумя точками на двумерной плоскости " + Math.sqrt(Math.pow(q,a)+ Math.pow(w,a)));
+       double a = 2;
+       p.x1 = 6;
+       p.y1 = 8;
+       p.x2 = 6;
+       p.y2 = 3;
+       double i = Math.sqrt(Math.pow((p.x1 - p.x2), a) + Math.pow((p.y1 - p.y2), a));
 
+          System.out.println("distance между двумя точками на двумерной плоскости" + i);
+   }
+    public static double distance(Point p){
 
-
-    }
+        return  (p.x1-p.y1)-(p.x2-p.y2);
+       }
 
 }
 
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,15 +1,12 @@
 package ru.stqa.pft.sandbox;
 
-public class Point  {
+public class Point {
 
-      public void Point1(double x1, double y1) {
-          x1 = 6;
-          y1 = 8;
-      }
+    public double x1;
+    public double y1;
+    public double x2;
+    public double y2;
 
 
-      public void Point2(double x2, double y2) {
-          x2 = 6;
-          y2 = 3;
-      }
 }
+
